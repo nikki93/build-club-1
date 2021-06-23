8b95c2ed566ed606af443c12c4ef8750e6406d84
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2021-06-22 21:04:35 -0700

    'core_rl' -> 'core_raylib', 'core_cp' -> 'core_chipmunk'

--- src/core.hh
+++ src/core.hh
@@ -4,8 +4,8 @@
 
 
 #include "core_precomp.hh"
-#include "core_rl.hh"
-#include "core_cp.hh"
+#include "core_raylib.hh"
+#include "core_chipmunk.hh"
 #include "core_meta.hh"
 #include "core_misc.hh"
 #include "core_entity.hh"
