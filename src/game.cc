927c114c4ad390e8f2e94df1a6337ef00ca3efd8
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2021-06-24 18:23:20 -0700

    committing the breakout game i made during the talk

--- src/game.cc
+++ src/game.cc
@@ -1,11 +1,116 @@
 #include "game.hh"
 
 
+constexpr float ballRadius = 10;
+
+constexpr float paddleY = 0.9 * tableSize.y;
+constexpr Vec2 paddleSize { 60, 12 };
+
+constexpr Vec2 brickSize { 50, 20 };
+constexpr int numBricksX = 10;
+constexpr int numBricksY = 5;
+
 void initGame() {
+  if (!loadEditSession()) {
+    auto xIncrement = tableSize.x / (numBricksX + 1);
+    for (auto i = 0; i < numBricksX; ++i) {
+      auto x = xIncrement * (i + 1);
+      game.bricks().push_back({
+          .pos { x, 60 },
+      });
+    }
+  }
 }
 
+float screenScale = 1;
+
 void updateGame(float dt) {
+  dprint("fps: %d", rl::GetFPS());
+
+  // Update screen scale
+  screenScale = rl::GetScreenWidth() / tableSize.x;
+
+  // Move paddle with mouse
+  game.paddleX() = rl::GetMousePosition().x / screenScale;
+
+  // Apply velocity to ball
+  game.ballPos() += game.ballVel() * dt;
+
+  // Bounce ball against walls
+  if (game.ballPos().y < ballRadius) {
+    game.ballPos().y = ballRadius;
+    game.ballVel().y *= -1;
+  }
+  if (game.ballPos().x < ballRadius) {
+    game.ballPos().x = ballRadius;
+    game.ballVel().x *= -1;
+  }
+  if (game.ballPos().y > tableSize.y - ballRadius) {
+    game.ballPos().y = tableSize.y - ballRadius;
+    game.ballVel().y *= -1;
+  }
+  if (game.ballPos().x > tableSize.x - ballRadius) {
+    game.ballPos().x = tableSize.x - ballRadius;
+    game.ballVel().x *= -1;
+  }
+
+  // Bounce ball against paddle
+  {
+    Vec2 paddlePos { game.paddleX(), paddleY };
+    auto paddleTopLeft = paddlePos - 0.5 * paddleSize;
+    rl::Rectangle paddleRect { paddleTopLeft.x, paddleTopLeft.y, paddleSize.x, paddleSize.y };
+    if (rl::CheckCollisionCircleRec(game.ballPos(), ballRadius, paddleRect)) {
+      auto delta = game.ballPos() - paddlePos;
+      delta /= paddleSize;
+      game.ballPos() -= game.ballVel() * dt;
+      if (std::abs(delta.x) > std::abs(delta.y)) {
+        game.ballVel().x *= -1;
+      } else {
+        game.ballVel().y *= -1;
+      }
+    }
+  }
+
+  // Bounce ball against bricks + destroy bricks
+  auto iter
+      = std::remove_if(game.bricks().begin(), game.bricks().end(), [&](GameState::Brick &brick) {
+          auto topLeft = brick.pos() - 0.5 * brickSize;
+          rl::Rectangle rect { topLeft.x, topLeft.y, brickSize.x, brickSize.y };
+          if (rl::CheckCollisionCircleRec(game.ballPos(), ballRadius, rect)) {
+            auto delta = game.ballPos() - topLeft;
+            delta /= brickSize;
+            game.ballPos() -= game.ballVel() * dt;
+            if (std::abs(delta.x) > std::abs(delta.y)) {
+              game.ballVel().x *= -1;
+            } else {
+              game.ballVel().y *= -1;
+            }
+            return true;
+          }
+          return false;
+        });
+  game.bricks().erase(iter, game.bricks().end());
 }
 
 void drawGame() {
+  rl::PushMatrix();
+
+  // Apply screen scale
+  rl::Scalef(screenScale, screenScale, 1);
+
+  // Draw ball
+  rl::DrawCircle(game.ballPos().x, game.ballPos().y, ballRadius, rl::RED);
+
+  // Draw paddle
+  Vec2 paddlePos { game.paddleX(), paddleY };
+  auto paddleTopLeft = paddlePos - 0.5 * paddleSize;
+  rl::DrawRectangle(paddleTopLeft.x, paddleTopLeft.y, paddleSize.x, paddleSize.y, rl::BLUE);
+
+  // Draw bricks
+  for (auto &brick : game.bricks()) {
+    auto topLeft = brick.pos() - 0.5 * brickSize;
+    rl::DrawRectangle(topLeft.x, topLeft.y, brickSize.x, brickSize.y, rl::YELLOW);
+  }
+
+  rl::PopMatrix();
 }
--- src/game.hh
+++ src/game.hh
@@ -11,7 +11,18 @@
 // State
 //
 
+constexpr Vec2 tableSize { 800, 450 };
+
 inline struct GameState {
+  Prop(Vec2, ballPos) { 0.5f * tableSize.x, 0.8f * tableSize.y };
+  Prop(Vec2, ballVel) { -160, -160 };
+
+  Prop(float, paddleX) { 0.5 * tableSize.x };
+
+  struct Brick {
+    Prop(Vec2, pos) { 0, 0 };
+  };
+  Prop(Seq<Brick>, bricks);
 } game;
 
 
